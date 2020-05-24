daf031340800aa630961d1699892721e65a1462f
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-23 23:05:15 -0700

    Reworked first error case.

--- TilesetCompiler.java
+++ TilesetCompiler.java
@@ -12,84 +12,83 @@ public class TilesetCompiler {
 	
 	public static void main(String[] args) {
 		System.out.println("* Tileset Compiler *");
-		if (args.length == 0 || args.length == 3) {
-			Scanner input = new Scanner(System.in);
-			String inputDirectoryLocation;
-			if (args.length == 0) {
-				System.out.print("Enter the input directory's location: ");
-				inputDirectoryLocation = input.nextLine();
-			} else {
-				inputDirectoryLocation = args[0];
-			}
-			String inputResolutionOption;
-			if (args.length == 0) {
-				System.out.println("Select the input resolution for the images:");
-				System.out.println("1) 8x8 pixels");
-				System.out.println("2) 16x16 pixels");
-				System.out.println("3) 32x32 pixels");
-				System.out.println("4) 64x64 pixels");
-				System.out.print("Input resolution option: ");
-				inputResolutionOption = input.nextLine();
-			} else {
-				inputResolutionOption = args[1];
-			}
-			String outputTilesetLocation;
-			if (args.length == 0) {
-				System.out.print("Enter the output tileset's location: ");
-				outputTilesetLocation = input.nextLine();
-			} else {
-				outputTilesetLocation = args[2];
-			}
-			input.close();
-			File inputDirectory = new File(inputDirectoryLocation);
-			if (inputDirectory.isDirectory()) {
-				if (isValidInteger(inputResolutionOption) && Integer.parseInt(inputResolutionOption) >= 1 &&
-						Integer.parseInt(inputResolutionOption) <= 4) {
-					inputLength = (int)Math.pow(2, Integer.parseInt(inputResolutionOption) + 2);
-					System.out.println("(Please wait a few seconds for the images to load.)");
-					ArrayList<BufferedImage> images = addImagesFromDirectory(inputDirectory);
-					BufferedImage tileset = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
-					int imageIndex = 0;
-					for (int y = 0; y < tileset.getHeight(); y += inputLength) {
-						for (int x = 0; x < tileset.getWidth(); x += inputLength) {
-							for (int imageY = 0; imageY < inputLength; imageY++) {
-								for (int imageX = 0; imageX < inputLength; imageX++) {
-									if (imageIndex < images.size()) {
-										tileset.setRGB(x + imageX, y + imageY,
-												images.get(imageIndex).getRGB(imageX, imageY));
-									} else {
-										tileset.setRGB(x + imageX, y + imageY, -1);
-									}
+		if (args.length != 0 && args.length != 3) {
+			error("This program's usage is as follows:\n" +
+					"java TilesetCompiler\n" +
+					"java TilesetCompiler <input directory location> <input resolution option> " +
+					"<output tileset location>");
+		}
+		Scanner input = new Scanner(System.in);
+		String inputDirectoryLocation;
+		if (args.length == 0) {
+			System.out.print("Enter the input directory's location: ");
+			inputDirectoryLocation = input.nextLine();
+		} else {
+			inputDirectoryLocation = args[0];
+		}
+		String inputResolutionOption;
+		if (args.length == 0) {
+			System.out.println("Select the input resolution for the images:");
+			System.out.println("1) 8x8 pixels");
+			System.out.println("2) 16x16 pixels");
+			System.out.println("3) 32x32 pixels");
+			System.out.println("4) 64x64 pixels");
+			System.out.print("Input resolution option: ");
+			inputResolutionOption = input.nextLine();
+		} else {
+			inputResolutionOption = args[1];
+		}
+		String outputTilesetLocation;
+		if (args.length == 0) {
+			System.out.print("Enter the output tileset's location: ");
+			outputTilesetLocation = input.nextLine();
+		} else {
+			outputTilesetLocation = args[2];
+		}
+		input.close();
+		File inputDirectory = new File(inputDirectoryLocation);
+		if (inputDirectory.isDirectory()) {
+			if (isValidInteger(inputResolutionOption) && Integer.parseInt(inputResolutionOption) >= 1 &&
+					Integer.parseInt(inputResolutionOption) <= 4) {
+				inputLength = (int)Math.pow(2, Integer.parseInt(inputResolutionOption) + 2);
+				System.out.println("(Please wait a few seconds for the images to load.)");
+				ArrayList<BufferedImage> images = addImagesFromDirectory(inputDirectory);
+				BufferedImage tileset = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+				int imageIndex = 0;
+				for (int y = 0; y < tileset.getHeight(); y += inputLength) {
+					for (int x = 0; x < tileset.getWidth(); x += inputLength) {
+						for (int imageY = 0; imageY < inputLength; imageY++) {
+							for (int imageX = 0; imageX < inputLength; imageX++) {
+								if (imageIndex < images.size()) {
+									tileset.setRGB(x + imageX, y + imageY,
+											images.get(imageIndex).getRGB(imageX, imageY));
+								} else {
+									tileset.setRGB(x + imageX, y + imageY, -1);
 								}
 							}
-							if (imageIndex < images.size()) {
-								imageIndex++;
-							}
+						}
+						if (imageIndex < images.size()) {
+							imageIndex++;
 						}
 					}
-					try {
-						ImageIO.write(tileset, "png", new FileOutputStream(outputTilesetLocation));
-						if (imageIndex == 1) {
-							System.out.println("Success: " + outputTilesetLocation + " was created from " +
-									imageIndex + " image!");
-						} else {
-							System.out.println("Success: " + outputTilesetLocation + " was created from " +
-									imageIndex + " images!");
-						}
-					} catch (Exception e) {
-						System.out.println("Error: Could not create " + outputTilesetLocation + ".");
+				}
+				try {
+					ImageIO.write(tileset, "png", new FileOutputStream(outputTilesetLocation));
+					if (imageIndex == 1) {
+						System.out.println("Success: " + outputTilesetLocation + " was created from " +
+								imageIndex + " image!");
+					} else {
+						System.out.println("Success: " + outputTilesetLocation + " was created from " +
+								imageIndex + " images!");
 					}
-				} else {
-					System.out.println("Error: Invalid input resolution option.");
+				} catch (Exception e) {
+					System.out.println("Error: Could not create " + outputTilesetLocation + ".");
 				}
 			} else {
-				System.out.println("Error: " + inputDirectory + " is not a valid directory.");
+				System.out.println("Error: Invalid input resolution option.");
 			}
 		} else {
-			System.out.println("This program's usage is as follows:");
-			System.out.println("java TilesetCompiler");
-			System.out.println("java TilesetCompiler <input directory location> <input resolution option> " +
-					"<output tileset location>");
+			System.out.println("Error: " + inputDirectory + " is not a valid directory.");
 		}
 	}
 	
