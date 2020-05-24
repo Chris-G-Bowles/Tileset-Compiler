080e7c71f539101aac9f0e51a0abbe4d98df1a08
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-24 01:32:37 -0700

    Removed isValidInteger
    The program will not compile as is until an additional push

--- TilesetCompiler.java
+++ TilesetCompiler.java
@@ -92,26 +92,6 @@ public class TilesetCompiler {
 		}
 	}
 	
-	private static boolean isValidInteger(String string) {
-		if (string.length() >= 2 && string.length() <= 10 && string.charAt(0) == '-') {
-			for (int i = 1; i < string.length(); i++) {
-				if (string.charAt(i) < '0' || string.charAt(i) > '9') {
-					return false;
-				}
-			}
-			return true;
-		} else if (string.length() >= 1 && string.length() <= 9) {
-			for (int i = 0; i < string.length(); i++) {
-				if (string.charAt(i) < '0' || string.charAt(i) > '9') {
-					return false;
-				}
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
 	private static ArrayList<BufferedImage> addImagesFromDirectory(File directory) {
 		File[] files = directory.listFiles();
 		ArrayList<BufferedImage> images = new ArrayList<>();
